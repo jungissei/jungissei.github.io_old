8744807968ac711b4786d3acf923035f00b210c6
jungiIssei <jungissei@gmail.com>
2021-12-06 07:31:24 +0900

    修正：グローバルメニュー

--- global_menu/scripts.js
+++ global_menu/scripts.js
@@ -1,41 +1,139 @@
-/* グローバルメニュー ここから */
-
-/*
-* 表示ページURLの第一階層と一致するグローバルメニューのアイテムに「.current」付与
-*/
-$(function(){
-  //現在のURLの第一階層文字列を取得
-  const cur_slug = location.pathname.match('(?<=/).*?(?=/)')[0];
-
-  //上記で取得した文字列に一致するグローバルメニューのアイテムに「.current」付与
-  $('#global_menu_items > li[data-slug]').each(function(){
-    let item_slug = $(this).attr('data-slug');
-    if(cur_slug == item_slug){
-      $(this).addClass('current');
-    }
-  });
-});
-
-
-/*
-* サブメニュー・メガメニュー表示
-*/
+/**------------------------------------------------------
+ * グローバルナビ：サブメニュー
+ *------------------------------------------------------*/
 $(window).on('load', function(){
-  //アイテムへのホバー時にサブメニュートグル
-  $('.js_toggle_by_hover').hover(function(){
-    $(this).children('.js_toggle_by_hover_content').slideDown(200).animate(
-      { opacity: 1 },
-      { queue: false, duration: 200 }
-    );
-  }, function(){
-    $(this).children('.js_toggle_by_hover_content').fadeOut(100).animate(
-      { opacity: 0 },
-      { queue: false, duration: 100 }
-    );
+
+  global_nav_sub_menu({
+    'show_class_name' : 'is_show',//追加クラス
+    'sub_menu_selector' : '#global_menu .mega_menu , #global_menu .accordion_menu',//サブメニューアイテムセレクター
+    'mouseleave_selector' : '#area_header'//初期化させるホバーアウトのセレクター
   });
+
+  $('body').removeClass('preload');
 });
 
+/**
+ * サブメニューアイテムにクラス名追加
+ * @param {string} arg
+ */
+function global_nav_sub_menu(arg){
+  let show_class_name = arg.show_class_name;
+  let sub_menu_selector = $(arg.sub_menu_selector);
+  let mouseleave_selector = $(arg.mouseleave_selector);
 
+  if(is_touch_display()){
+    let flag = true;
+
+    sub_menu_selector.on('click', function(){
+
+      if(flag != true) return;
+      flag = false;
+      setTimeout(function(){ flag = true; }, 500);
+
+      toggle_class_global_nav_sub_menu({
+        sub_menu_selector : sub_menu_selector,
+        show_class_name : show_class_name,
+        event_elem : this
+      });
+    });
+
+  }else{
+    sub_menu_selector.on('mouseover', function(){
+
+      add_class_global_nav_sub_menu({
+        sub_menu_selector : sub_menu_selector,
+        show_class_name : show_class_name,
+        event_elem : this
+      });
+    });
+
+  }
+
+  mouseleave_selector.on('mouseleave', function(){
+    initialize_global_nav_sub_menu({
+      sub_menu_selector : sub_menu_selector,
+      show_class_name : show_class_name
+    });
+  });
+}
+
+
+
+/**
+ * サブメニュー初期化
+ * @param {object} arg
+ */
+function initialize_global_nav_sub_menu(arg){
+  let sub_menu_selector = arg.sub_menu_selector;
+  let show_class_name = arg.show_class_name;
+  let escape_event_elem = arg.escape_event_elem;
+
+  sub_menu_selector.each(function(){
+    if(escape_event_elem && this == escape_event_elem){
+      return;
+    }
+
+    $(this).removeClass(show_class_name);
+  });
+}
+
+
+
+/**
+ * タッチ対応ディスプレイの真偽を返す
+ * @returns {bool}
+ */
+function is_touch_display(){
+  let touch_event = window.ontouchstart;
+  let touch_points = navigator.maxTouchPoints;
+
+  if(
+    touch_event !== undefined
+    && 0 < touch_points
+  ) {
+    return true;
+  }
+
+  return false;
+}
+
+
+/**
+ * クリックイベントのトグルでサブメニュー表示切替
+ * @param {object} params
+ */
+function toggle_class_global_nav_sub_menu(params) {
+  //イベント要素以外初期化
+  initialize_global_nav_sub_menu({
+    sub_menu_selector : params.sub_menu_selector,
+    show_class_name : params.show_class_name,
+    escape_event_elem : params.event_elem
+  });
+
+  $(params.event_elem).toggleClass(params.show_class_name);
+}
+
+/**
+ * マウスオーバーイベントでサブメニュー表示
+ * @param {object} params
+ */
+function add_class_global_nav_sub_menu(params) {
+  //イベント要素以外初期化
+  initialize_global_nav_sub_menu({
+    sub_menu_selector : params.sub_menu_selector,
+    show_class_name : params.show_class_name,
+    escape_event_elem : params.event_elem
+  });
+
+  $(params.event_elem).addClass(params.show_class_name);
+}
+
+
+
+
+/**------------------------------------------------------
+ * エリアヘッダー：スクロール追従
+ *------------------------------------------------------*/
 /*
 * スクロール追従
 */
