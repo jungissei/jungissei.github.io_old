7089d9ef987281f7b685140f19efd9f640077838
Jung Issei <42998753+jungissei@users.noreply.github.com>
2021-12-07 14:00:27 +0900

    appendさせたアンカーリンクを動かすため、onload内に設置 #30

--- smooth_scroll/scripts.js
+++ smooth_scroll/scripts.js
@@ -1,50 +1,47 @@
-/* スムーズスクロール ここから */
+/*----------------------------------------------------------------------------
+ * スムーズスクロール
+ *----------------------------------------------------------------------------*/
+$(window).on('load', function(){
+  smooth_scroll(
+    $( location ).attr('hash')
+  );
 
-/*
-* スムーズスクロール
-* @param string ハッシュ値
-*/
+  //appendさせたアンカーリンクを動かすため、onload内に設置
+  $('a[href*="#"]').click(function(){
+    smooth_scroll(
+      $(this)[0].hash
+    );
+    return false;
+  });
+});
+
+/**
+ * スムーズスクロール
+ * @param {string} ハッシュ値
+ */
 function smooth_scroll(hash){
   //動作条件
   if(is_available_for_smooth_scroll(hash)){
-    jQuery('html, body').animate(
-      { scrollTop: jQuery(hash).offset().top },
+    $('html, body').animate(
+      { scrollTop: $(hash).offset().top },
       500,
       'swing'
     );
   }
 }
 
-/*
-* スムーズスクロール 動作条件
-* @param string ハッシュ値
-* @return bool available or not
-*/
+/**
+ * @param {string} ハッシュ値
+ * @return bool スムーズスクロール 動作条件
+ */
 function is_available_for_smooth_scroll(hash){
   //ハッシュ値が空の時
   if(hash == '#' || hash == '')
     return false;
 
   //ターゲット要素が存在しない時
-  if(jQuery(hash).length == 0)
+  if($(hash).length == 0)
     return false;
 
   return true;
 }
-
-//ページを読込後
-jQuery(window).on('load', function(){
-  smooth_scroll(
-    jQuery( location ).attr('hash')
-  );
-});
-
-//クリック後
-jQuery('a[href*="#"]').click(function(){
-  smooth_scroll(
-    jQuery(this)[0].hash
-  );
-  return false;
-});
-
-/* スムーズスクロール ここまで */
