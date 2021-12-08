7cfe693b735028843a132bdd2d3337314d8c764d
jungiIssei <jungissei@gmail.com>
2021-12-09 07:17:51 +0900

    ハンバーガーメニュー修正 #2

--- hamburger_menu/scripts.js
+++ hamburger_menu/scripts.js
@@ -1,108 +1,83 @@
 /*----------------------------------------------------------------------------
  * ハンバーガーメニュー
  *----------------------------------------------------------------------------*/
-$(function(){
-  let args = {
-    'btn_selector' : $('#hamburger_menu > .menu_btn'),
-    'body_class' : '.hamburger_menu_body',
-    'open_class' : 'is_opened'
-  };
 
-  let flag = true;
-  $("#hamburger_menu > .menu_btn").on('click', function(){
-    if(flag == false) return;
-    flag = false;
-    setTimeout(function(){ flag = true; }, 500);
-
-    // args = { 'scroll_control' : true }
-    toggle_hamburger_menu(args);
-  });
-
-  $("#hamburger_menu > .hamburger_menu_body a[href]").on('click', function(){
-    if($(this).attr('target')) return;
-
-    // args = { 'scroll_control' : false }
-    toggle_hamburger_menu(args);
-  });
-});
-
-
-
-/**
- * ハンバーガーメニューボタン：トグル
- * @param {object} 設定情報
- */
-function toggle_hamburger_menu(args){
-  let btn = args.btn_selector;
-  btn.toggleClass(args.open_class);
-  btn.next('.hamburger_menu_body').fadeToggle();
-
-  let menu_body_opened = (btn.hasClass(args.open_class))? true: false;
-  scroll_control_hamburger_menu(menu_body_opened);
-}
-
-
-
-/**
- * ハンバーガーメニュー開閉時：スクロールコントール
- * @param {bool} menu_body_opened
- */
-function scroll_control_hamburger_menu(menu_body_opened){
-  if(menu_body_opened){
-    //ページ全体のスクロール無効化
-    disable_scroll_body();
-    return;
+class hamburger_menu{
+  constructor() {
+    this.btn_selector = $('#hamburger_menu > .menu_btn');//ハンバーガーメニューボタンセレクタ
+    this.anchor_selector = $('#hamburger_menu > .hamburger_menu_body a[href]');//ハンバーガーメニューアンカーセレクタ
+    this.body_class = '.hamburger_menu_body';//ハンバーガーメニューbodyクラス
+    this.open_class = 'is_opened';//開閉クラス
   }
 
-  //ページ全体のスクロール無効化解除
-  enable_scroll_body();
-}
+  open_menu(){
+    let self = this;
+    let flag = true;
+    this.btn_selector.on('click', function(){
+      if(flag == false) return;
+      flag = false;
+      setTimeout(function(){ flag = true; }, 500);
 
-/**
- * ページ全体のスクロール無効化
- */
-function disable_scroll_body() {
-  $('body').css({
-    'overflow' : 'hidden',
-    'position' : 'fixed',
-    'top' : - $(window).scrollTop()
-  });
+      self.toggle_body();
+    });
 
-  document.addEventListener(
-    'touchmove',
-    scroll_control_for_touchmove,
-  );
-}
+    this.anchor_selector.on('click', function(){
+      if($(this).attr('target')) return;
 
-/**
- * ページ全体のスクロール無効化解除
- */
-function enable_scroll_body() {
-  let scroll_y = document.body.style.top;
-
-  $('body').css({
-    'overflow' : '',
-    'position' : '',
-    'top' : ''
-  });
-
-  window.scrollTo(0, parseInt(scroll_y || '0') * -1);
-
-  document.removeEventListener(
-    'touchmove',
-    scroll_control_for_touchmove,
-  );
-}
-
-/**
- * touchmoveイベントコントロール
- * @param {object} イベントオブジェクト
- */
-function scroll_control_for_touchmove(e) {
-  if ($(e.target).closest('.hamburger_menu_body').length) {
-    e.stopPropagation();
-    return;
+      self.toggle_body();
+    });
   }
 
-  e.preventDefault();
-};
+  /**
+   * ハンバーガーメニューボタン：トグル
+   */
+  toggle_body(){
+    this.btn_selector.toggleClass(this.open_class);
+    this.btn_selector.next(this.body_class).fadeToggle();
+
+    this.scroll_control();
+  }
+
+  /**
+   * ハンバーガーメニュー開閉時：スクロールコントール
+   */
+  scroll_control(){
+    if(this.btn_selector.hasClass(this.open_class)){
+      //ページ全体のスクロール無効化
+      this.disable_body_scroll();
+      return;
+    }
+
+    //ページ全体のスクロール無効化解除
+    this.enable_body_scroll();
+  }
+
+  /**
+   * ページ全体のスクロール無効化
+   */
+  disable_body_scroll() {
+    $('body').css({
+      'padding-right' : '15px',
+      'position' : 'fixed',
+      'top' : - $(window).scrollTop()
+    });
+  }
+
+  /**
+   * ページ全体のスクロール無効化解除
+   */
+  enable_body_scroll() {
+    let scroll_y = document.body.style.top;
+
+    $('body').css({
+      'padding-right' : '',
+      'position' : '',
+      'top' : ''
+    });
+
+    window.scrollTo(0, parseInt(scroll_y || '0') * -1);
+  }
+}
+
+let hamburger_menu_class = new hamburger_menu();
+hamburger_menu_class.open_menu();
