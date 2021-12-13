d83beea858e28b876ec8af09da2b399d01ae3828
jungiIssei <jungissei@gmail.com>
2021-12-14 07:14:41 +0900

    fix/スムーススクロール：ヘッダー固定時を考慮した座標で止まるようにする #26

--- smooth_scroll/scripts.js
+++ smooth_scroll/scripts.js
@@ -2,14 +2,22 @@
  * スムーズスクロール
  *----------------------------------------------------------------------------*/
 $(window).on('load', function(){
+
+  let arg = {
+    header_fixed_height_pc : 70,//PC時の固定ヘッダーの高さ
+    header_fixed_height_sp : 50//SP時の固定ヘッダーの高さ
+  };
+
   smooth_scroll(
-    $( location ).attr('hash')
+    $( location ).attr('hash'),
+    arg
   );
 
   //appendさせたアンカーリンクを動かすため、onload内に設置
   $('a[href*="#"]').click(function(){
     smooth_scroll(
-      $(this)[0].hash
+      $(this)[0].hash,
+      arg
     );
     return false;
   });
@@ -17,13 +25,17 @@ $(window).on('load', function(){
 
 /**
  * スムーズスクロール
- * @param {string} ハッシュ値
+ * @param {string} hash ハッシュ値
+ * @param {object} arg 設定値オブジェクト
  */
-function smooth_scroll(hash){
+function smooth_scroll(hash, arg){
   //動作条件
   if(is_available_for_smooth_scroll(hash)){
+
+    let adjust_scroll_top = get_adjust_scroll_top(hash, arg);
+
     $('html, body').animate(
-      { scrollTop: $(hash).offset().top },
+      { scrollTop: $(hash).offset().top - adjust_scroll_top},
       500,
       'swing'
     );
@@ -31,8 +43,8 @@ function smooth_scroll(hash){
 }
 
 /**
- * @param {string} ハッシュ値
- * @return bool スムーズスクロール 動作条件
+ * @param {string} hash ハッシュ値
+ * @return {bool} スムーズスクロール 動作条件
  */
 function is_available_for_smooth_scroll(hash){
   //ハッシュ値が空の時
@@ -44,4 +56,18 @@ function is_available_for_smooth_scroll(hash){
     return false;
 
   return true;
+}
+
+/**
+ * @param {string} hash ハッシュ値
+ * @param {object} arg 設定値オブジェクト
+ * @return {int} スクロール停止位置の調整値
+ */
+function get_adjust_scroll_top(hash, arg) {
+  if($(hash).hasClass('esc_adjust_scroll_top')){
+    return 0;
+  }else{
+    return window.matchMedia( "(max-width: 768px)" ).matches?
+      arg.header_fixed_height_pc : arg.header_fixed_height_sp;
+  }
 }
