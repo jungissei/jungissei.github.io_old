b24a9bc1db010923643249c48b18bc9869de212d
JungIssei <jungissei@gmail.com>
2022-03-02 20:30:40 +0900

    refactor：タブメニュー4

--- tab_menu/scripts.js
+++ tab_menu/scripts.js
@@ -1,21 +1,11 @@
 /**
  * タブメニュー
  */
-$(function(){
+$('#tab_menu .menu_btn li').on('click', function(){
 
-  let flag = true;
+  let index = $('#tab_menu .menu_btn li').index(this);
+  $('#tab_menu .menu_btn li').removeClass('is_active');
+  $(this).addClass('is_active');
+  $('#tab_menu .menu_body .menu_items').removeClass('is_show').eq(index).addClass('is_show');
 
-  $('#tab_menu .menu_btn li').on('click', function(){
-
-    if(flag == false) return;
-    flag = false;
-    setTimeout(function(){ flag = true; }, 500);
-
-    let index = $('#tab_menu .menu_btn li').index(this);
-    $('#tab_menu .menu_btn li').removeClass('is_active');
-    $(this).addClass('is_active');
-    $('#tab_menu .menu_body .menu_items').removeClass('is_show').eq(index).addClass('is_show');
-
-  });
 });
-
