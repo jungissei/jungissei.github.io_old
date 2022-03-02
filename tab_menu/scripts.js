2536fedc5cafc032cce4bd559072cede614bc565
JungIssei <jungissei@gmail.com>
2022-03-02 17:54:34 +0900

    refactor：タブメニュー

--- tab_menu/scripts.js
+++ tab_menu/scripts.js
@@ -1,9 +1,21 @@
-/* タブメニュー ここから */
-$('#tab_menu .tab_menu_btn li').on('click', function(){
-  let index = $('#tab_menu .tab_menu_btn li').index(this);
-  $('#tab_menu .tab_menu_btn li').removeClass('active');
-  $(this).addClass('active');
-  $('#tab_menu .tab_menu_body_wrp .tab_menu_body').removeClass('show').eq(index).addClass('show');
+/**
+ * タブメニュー
+ */
+$(function(){
 
+  let flag = true;
+
+  $('#tab_menu .menu_btn li').on('click', function(){
+
+    if(flag == false) return;
+    flag = false;
+    setTimeout(function(){ flag = true; }, 500);
+
+    let index = $('#tab_menu .menu_btn li').index(this);
+    $('#tab_menu .menu_btn li').removeClass('active');
+    $(this).addClass('active');
+    $('#tab_menu .menu_body .menu_items').removeClass('is_show').eq(index).addClass('is_show');
+
+  });
 });
-/* タブメニュー ここまで */
+
